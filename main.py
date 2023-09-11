b9558f6182ed64e3f92277a901faa4e2d6f5f611
pab1m <pavlosenchak14@gmail.com>
2023-09-11 18:48:49 +0300

    done 3.0

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import re
 
 
-def check(check_text):
+def check_uk_text(check_text):
     pattern = re.compile('^[а-яїєґі\s.,!?-]+$')
     return bool(pattern.match(check_text.lower()))
 
 
-text_1 = input("Введіть текст: ")
-
-if check(text_1):
-    print("Рядок є україномовним")
-
+def check_diff(txt_1):
     text_2 = input("Введіть текст 2: ")
-
-    if len(text_1) > len(text_2):
-        print(f"Рядок 1 більший за рядок 2 на '{len(text_1)-len(text_2)}'")
-
-    elif len(text_2) > len(text_1):
-        print(f"Рядок 2 більший за рядок 1 на '{len(text_2)-len(text_1)}'")
-
+    if len(txt_1) > len(text_2):
+        return print(f"Рядок 1 більший за рядок 2 на '{len(txt_1)-len(text_2)}'")
+    elif len(text_2) > len(txt_1):
+        return print(f"Рядок 2 більший за рядок 1 на '{len(text_2)-len(txt_1)}'")
     else:
-        print("Рядки є однаковими")
+        return print("Рядки є однаковими")
+
+
+text_1 = input("Введіть текст: ")
+if check_uk_text(text_1):
+    print("Рядок є україномовним")
+    check_diff(text_1)
+else:
+    print("Рядок не є україномовним")
