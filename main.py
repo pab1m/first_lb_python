9eb62d241fbdfc0c6c43c11e942895bcc4c09033
pab1m <pavlosenchak14@gmail.com>
2023-09-11 19:49:47 +0300

    done 4.0

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ def check_uk_text(check_text):
 
 def check_diff(txt_1):
     text_2 = input("Введіть текст 2: ")
-    if len(txt_1) > len(text_2):
-        return print(f"Рядок 1 більший за рядок 2 на '{len(txt_1)-len(text_2)}'")
-    elif len(text_2) > len(txt_1):
-        return print(f"Рядок 2 більший за рядок 1 на '{len(text_2)-len(txt_1)}'")
+    if check_uk_text(text_2):
+        if len(txt_1) > len(text_2):
+            return print(f"Рядок 1 більший за рядок 2 на '{len(txt_1)-len(text_2)}'")
+        elif len(text_2) > len(txt_1):
+            return print(f"Рядок 2 більший за рядок 1 на '{len(text_2)-len(txt_1)}'")
+        else:
+            return print("Рядки є однаковими")
     else:
-        return print("Рядки є однаковими")
+        return print("Рядок не є україномовним")
 
 
 text_1 = input("Введіть текст: ")
